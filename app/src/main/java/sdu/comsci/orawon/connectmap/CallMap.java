8e125a70c2ad1071da14974d858c0eec2e3af0fc
aornpl <aororawonnonpala@gmail.com>
2017-04-20 11:49:55 +0700

    Test Map

--- app/src/main/java/sdu/comsci/orawon/connectmap/CallMap.java
+++ app/src/main/java/sdu/comsci/orawon/connectmap/CallMap.java
@@ -1,7 +1,11 @@
 package sdu.comsci.orawon.connectmap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CallMap extends AppCompatActivity {
 
@@ -12,4 +16,21 @@ public class CallMap extends AppCompatActivity {
     } //onCreate Method
 
 
+    public void runCallMap(View view) {
+
+        //Initial View
+        EditText latitude = (EditText) findViewById(R.id.mapLat);
+        EditText longtitude = (EditText) findViewById(R.id.mapLong);
+        EditText label = (EditText) findViewById(R.id.mapLabel);
+
+        String lat = latitude.getText().toString().trim();
+        String lng = longtitude.getText().toString().trim();
+        String mlabel = label.getText().toString().trim();
+
+        Uri location = Uri.parse("http://maps.google.com/maps?z=10&q=loc:13.759774,100.541525");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+        //ใช้ไม่ได้กับจินนี่โมชั่น mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+
+    } //runcallmap
 } //Main Class
